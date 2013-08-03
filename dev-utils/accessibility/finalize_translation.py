1b8a2629eac32228d5d91ddd991cff78d3b926d5
Sebastián J. Seba <ssebastianj@gmail.com>
2013-08-03 19:55:04 -0300

    Added Python 2 & Python 3 support in same codebase.
    Signed-off-by: Sebastián J. Seba <ssebastianj@gmail.com>

--- dev-utils/accessibility/finalize_translation.py
+++ dev-utils/accessibility/finalize_translation.py
@@ -1,73 +1,108 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
 
 import glob
 import shutil
 import sys
 import os
 
-tran_type=''
-active_lang=''
+PY2 = sys.version_info[0] == 2
+if not PY2:
+    text_type = str
+    string_types = (str,)
+    unichr = chr
+
+    def u(s):
+        return s
+
+    def b(s):
+        return s.encode('utf-8')
+else:
+    text_type = unicode
+    string_types = (str, unicode)
+    unichr = unichr
+
+    def u(s):
+        return unicode(s, 'unicode_escape')
+
+    def b(s):
+        return s
+
+tran_type = ''
+active_lang = ''
+root_dir = par_dir = os.path.dirname(os.path.dirname(
+                                     os.path.dirname(
+                                     os.path.abspath(__file__))))
 
 if len(sys.argv) == 2 and sys.argv[1] == 'localize':
-	tran_type=sys.argv[1]
+    tran_type=sys.argv[1]
 elif len(sys.argv) == 3 and sys.argv[1] == 'internationalize':
-	tran_type=sys.argv[1]
-	active_lang=sys.argv[2]
+    tran_type=sys.argv[1]
+    active_lang=sys.argv[2]
 else:
-	sys.stderr.write('Usage: %s localize\n' % sys.argv[0])
-	sys.stderr.write('  example: %s internationalize English-EN\n' % sys.argv[0])
-	sys.exit(1)
-	
-#if localize, ensure hidden .config file is devoid of i18n
-#if internationalize, ensure the hidden .config file contains plugin-gargoyle-i18n and the target language package
-#if os.path.exists('./package/plugin-gargoyle-i18n/files/etc/uci-defaults/zzz-plugin-gargoyle-i18n'):
+    sys.stderr.write('Usage: {0} localize\n'.format(sys.argv[0]))
+    sys.stderr.write('  example: {0} internationalize English-EN\n'.format(sys.argv[0]))
+    sys.exit(1)
+
+# if localize, ensure hidden .config file is devoid of i18n
+# if internationalize, ensure the hidden .config file contains plugin-gargoyle-i18n and the target language package
+# if os.path.exists('./package/plugin-gargoyle-i18n/files/etc/uci-defaults/zzz-plugin-gargoyle-i18n'):
 for config_file in glob.glob('./*-src/.config'):
-	#there should be only one
-	if tran_type=='internationalize':
-		print ('Editing config file to build in %s translation\n' % (active_lang,))
-		
-	cfg_fileFO = open(config_file, 'rb')
-	cfg_doc=cfg_fileFO.readlines()
-	cfg_fileFO.close()
-	
-	newcfg_doc=[]
-	for cline in cfg_doc:
-		anewline=''
-		
-		if cline.startswith('CONFIG_PACKAGE_plugin-gargoyle-i18n') and tran_type=='localize' :
-			anewline='# '+cline
-		if cline.startswith('CONFIG_PACKAGE_plugin-gargoyle-i18n') and tran_type=='internationalize' :
-			#sorry, but I'm sure your some slick brotha and you've got mad dope skillz, but...
-			anewline='# '+cline
-	
-		if anewline != '':
-			newcfg_doc.append(anewline)
-		else:
-			newcfg_doc.append(cline)
-			
-	if tran_type=='internationalize':
-		newcfg_doc.append('#\n')
-		newcfg_doc.append('# Gargoyle I18N\n')
-		newcfg_doc.append('#\n')
-		newcfg_doc.append('\n')
-		newcfg_doc.append('CONFIG_PACKAGE_plugin-gargoyle-i18n=y\n')
-		found_lang=False
-		for langpack in glob.glob('./package/plugin-gargoyle-i18n-*/files/www/i18n/*'):
-			lang=os.path.basename(langpack)
-			if lang==active_lang:
-				found_lang=True
-				newcfg_doc.append('CONFIG_PACKAGE_plugin-gargoyle-i18n-%s=y\n' % (lang,))
-			else:
-				newcfg_doc.append('CONFIG_PACKAGE_plugin-gargoyle-i18n-%s=m\n' % (lang,))
-				
-		newcfg_doc.append('\n')
-		
-		if found_lang == False:
-			sys.stderr.write('finalize was unable to find the target language\n')
-			sys.exit(1)
-		
-	cfg_fileFO = open(config_file, 'wb')
-	cfg_fileFO.seek(0)
-	cfg_fileFO.writelines(newcfg_doc)
-	cfg_fileFO.close()
-		
+    # there should be only one
+    if tran_type == 'internationalize':
+        print('Editing config file to build in {0} translation\n'.format(active_lang))
+
+    with open(config_file, 'rb') as cfg_fileFO:
+        cfg_doc=cfg_fileFO.readlines()
+
+    newcfg_doc = []
+    for cline in cfg_doc:
+        anewline = ''
+
+        if cline.startswith('CONFIG_PACKAGE_plugin-gargoyle-i18n') and tran_type == 'localize':
+            anewline = '# ' + cline
+        if cline.startswith('CONFIG_PACKAGE_plugin-gargoyle-i18n') and tran_type == 'internationalize':
+            # sorry, but I'm sure your some slick brotha and you've got mad dope skillz, but...
+            anewline= '# ' + cline
+
+        if anewline != '':
+            newcfg_doc.append(anewline)
+        else:
+            newcfg_doc.append(cline)
+
+    if tran_type == 'internationalize':
+        newcfg_doc.append('#\n')
+        newcfg_doc.append('# Gargoyle I18N\n')
+        newcfg_doc.append('#\n')
+        newcfg_doc.append('\n')
+        newcfg_doc.append('CONFIG_PACKAGE_plugin-gargoyle-i18n=y\n')
+
+        found_lang = False
+        search_path = os.path.join(root_dir,
+                                  'package',
+                                  'plugin-gargoyle-i18n-*',
+                                  'files',
+                                  'www',
+                                  'i18n',
+                                  '*')
+
+        for langpack in glob.glob(search_path):
+            lang = os.path.basename(langpack)
+
+            if lang == active_lang:
+                found_lang = True
+                newcfg_doc.append('CONFIG_PACKAGE_plugin-gargoyle-i18n-{0}=y\n'.format(lang))
+            else:
+                newcfg_doc.append('CONFIG_PACKAGE_plugin-gargoyle-i18n-{0}=m\n'.format(lang))
+
+        newcfg_doc.append('\n')
+
+        if not found_lang:
+            sys.stderr.write('finalize was unable to find the target language\n')
+            sys.exit(1)
+
+    with open(config_file, 'wb') as cfg_fileFO:
+        cfg_fileFO.seek(0)
+        cfg_fileFO.writelines(newcfg_doc)
--- dev-utils/accessibility/internationalize.py
+++ dev-utils/accessibility/internationalize.py
@@ -1,49 +1,95 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+    # -*- coding: utf-8 -*-
 
+from __future__ import print_function, unicode_literals
+
+import os
 import shutil
 import sys
-import os
 
-act_lang=''
+
+PY2 = sys.version_info[0] == 2
+if not PY2:
+    text_type = str
+    string_types = (str,)
+    unichr = chr
+
+    def u(s):
+        return s
+
+    def b(s):
+        return s.encode('utf-8')
+else:
+    text_type = unicode
+    string_types = (str, unicode)
+    unichr = unichr
+
+    def u(s):
+        return unicode(s, 'unicode_escape')
+
+    def b(s):
+        return s
+
+act_lang = ''
+root_dir = par_dir = os.path.dirname(os.path.dirname(
+                                     os.path.dirname(
+                                     os.path.abspath(__file__))))
 
 if len(sys.argv) == 2:
-	act_lang=sys.argv[1]
-	if not os.path.exists('./package/plugin-gargoyle-i18n-%s/files/www/i18n/%s/menus.txt' % (act_lang,act_lang)):
-		print 'Error: target language package not found.'
-		sys.exit(1)
-else:
-	sys.stderr.write('Usage: %s active_language\n' % sys.argv[0])
-	sys.stderr.write('  example: %s English-EN\n' % sys.argv[0])
-	
-shutil.copytree('./package', './package-orig')
+    act_lang = sys.argv[1]
+    menu_path = os.path.join(root_dir,
+                            'package',
+                            'plugin-gargoyle-i18n-{0}'.format(act_lang),
+                            'files',
+                            'www',
+                            'i18n'
+                            '{1}'.format(act_lang),
+                            'menus.txt')
 
-if os.path.exists('./package/plugin-gargoyle-i18n/files/etc/uci-defaults/zzz-plugin-gargoyle-i18n'):
-	print 'Setting target language'
-	uci_fileFO = open('./package/plugin-gargoyle-i18n/files/etc/uci-defaults/zzz-plugin-gargoyle-i18n', 'rb')
-	ucipage=uci_fileFO.readlines()
-	uci_fileFO.close()
-	new_ucipage_contents=[]
-	
-	for uciline in ucipage:
-		anewline=''
-		if uciline.startswith('uci set gargoyle.global.fallback_lang'):
-			anewline=('uci set gargoyle.global.fallback_lang=%s\n' % (act_lang,))
-		if uciline.startswith('uci set gargoyle.global.language'):
-			anewline=('uci set gargoyle.global.language=%s\n' % (act_lang,))
-		if uciline.startswith('change_menu_language'):
-			anewline=('change_menu_language "%s"\n' % (act_lang,))
-		
-		if anewline != '':
-			new_ucipage_contents.append(anewline)
-		else:
-			new_ucipage_contents.append(uciline)
-			
-	out_uci_fileFO = open('./package/plugin-gargoyle-i18n/files/etc/uci-defaults/zzz-plugin-gargoyle-i18n', 'wb')
-	out_uci_fileFO.seek(0)
-	out_uci_fileFO.writelines(new_ucipage_contents)
-	out_uci_fileFO.close()
-	
+    if not os.path.exists(menu_path):
+        print('Error: target language package not found.')
+        sys.exit(1)
 else:
-	print 'ERROR: the default language settings cannot be set. Pages will not render.'
-	sys.exit(1)
-	
+    sys.stderr.write('Usage: {0} active_language\n'.format(sys.argv[0]))
+    sys.stderr.write('  example: {0} English-EN\n'.format(sys.argv[0]))
+
+pkg_dir = os.path.join(root_dir, 'package')
+pkg_orig_dir = os.path.join(root_dir, 'package-orig')
+shutil.copytree(pkg_dir, pkg_orig_dir)
+
+i18n_path = os.path.join(root_dir,
+                         'package',
+                         'plugin-gargoyle-i18n',
+                         'files',
+                         'etc',
+                         'uci-defaults',
+                         'zzz-plugin-gargoyle-i18n')
+
+if os.path.exists(i18n_path):
+    print('Setting target language')
+
+    with open(i18n_path, 'rb') as uci_fileFO:
+        ucipage=uci_fileFO.readlines()
+        new_ucipage_contents=[]
+
+    for uciline in ucipage:
+        anewline = ''
+
+        if uciline.startswith(bytearray(b'uci set gargoyle.global.fallback_lang')):
+            anewline = u('uci set gargoyle.global.fallback_lang={0}\n'.format(act_lang))
+        if uciline.startswith(bytearray(b'uci set gargoyle.global.language')):
+            anewline = u('uci set gargoyle.global.language={0}\n'.format(act_lang))
+        if uciline.startswith(bytearray(b'change_menu_language')):
+            anewline = u('change_menu_language "{0}"\n'.format(act_lang))
+
+        if anewline != '':
+            new_ucipage_contents.append(anewline)
+        else:
+            new_ucipage_contents.append(uciline)
+
+    with open(i18n_path, 'wb') as out_uci_fileFO:
+        out_uci_fileFO.seek(0)
+        out_uci_fileFO.writelines(new_ucipage_contents)
+else:
+    print('ERROR: the default language settings cannot be set. Pages will not render.')
+    sys.exit(1)
